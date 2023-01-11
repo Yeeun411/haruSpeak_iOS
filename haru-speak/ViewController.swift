c829fe5b463fab05f9e97578bb61bf01a283ff15
Yeaeun411 <yeaeun411@naver.com>
2023-01-11 15:28:19 +0900

    test commit3

--- haru-speak/ViewController.swift
+++ haru-speak/ViewController.swift
@@ -12,8 +12,6 @@ import UIKit
 class ViewController: UIViewController {
     
     
-    
-
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
